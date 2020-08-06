dbebe05fa843b02e59d34ec6368aed4e73995538
haijee <haijee@qq.com>
2020-08-06 16:03:24 +0800

    build

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Hello, my friend!',
+    // base: '/docs/',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: `/favicon.ico`
+        }]
+    ],
+    dest: './docs/.vuepress/dist',
+    ga: '',
+    evergreen: true,
+}
